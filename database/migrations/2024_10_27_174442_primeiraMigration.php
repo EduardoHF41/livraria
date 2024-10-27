5bc94933a209ccdd55a2beef9d2139d32f92e229
EduardoHF41 <1210eduardoxx@gmail.com>
2024-10-27 15:49:01 -0300

    primeiro commit

---
+++ database/migrations/2024_10_27_174442_primeiraMigration.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->boolean('admin')->default(false);
+            $table->timestamps();
+        });
+        schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->float('price');
+            $table->string('synopsis');
+            $table->binary('image');
+            $table->string('category');
+            $table->integer('ISBN');
+            $table->string('description');
+            $table->integer('quantity');
+            $table->date('publication_date');
+            $table->string('edition');
+            $table->timestamps();
+        });
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('book_id')->constrained('books')->onDelete('cascade');
+            $table->unsignedTinyInteger('rating')->default(0);
+            $table->text('comment')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('books');
+        Schema::dropIfExists('reviews');
+    }
+};
